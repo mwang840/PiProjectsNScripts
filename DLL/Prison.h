9b41360af30f704c0b6bc48196f4f73f95636566
mwang840 <maxwang@udel.edu>
2022-06-10 21:28:06 -0400

    similar of what I did in the .h file

--- DLL/Prison.h
+++ DLL/Prison.h
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Prisoner {
-    Prisoner *next;
-    Prisoner *prev;
+    struct Prisoner *next;
+    struct Prisoner *prev;
     int age;
     char *name;
 };
 
-void push(char *name, int age, char *prisonId);
-Prisoner pop();
+void push(char *name, int age, char *prisonId, int sentence, char *crime);
+struct Prisoner *pop();
 int remove(char *prisonId);
 void sentence(char *name);
+void printlist();
