03dc16fcc0623c21849e1c2d0db22c6e91485661
mwang840 <maxwang@udel.edu>
2022-07-01 17:35:34 -0400

    should be able to print the lists attributes

--- DLL/Prison.c
+++ DLL/Prison.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Prison.h"
+#include "prison.h"
 
 void push(char *name, int age, char *prisonId, int sentence, char *crimes);
 int remove(char *prisonId);
 void sentence(char *name);
 void printlist();
+
 int main(){
     printf("Hello");
     return 0;
 }
 
-void printlist(){
-    return;
+void printlist(Prisoner *p){
+    Prison *temp = p;
+    while(temp != NULL){
+        printf("Prisoner name is %s, is %d years old, has an id of %s, is in jail for %d years due to the crimes of %s he committed", temp->name, temp->age, temp->prisonId, temp->sentence, temp->crimes);
+        temp = temp->next;
+    }
 }
